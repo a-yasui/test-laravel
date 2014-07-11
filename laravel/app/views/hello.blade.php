aa8666b778c1a1f72f5f1b98d7b2ff92eb14ce84
Atsushi Yasui @ MBA2 <a.yasui@gmail.com>
2014-07-11 18:42:39 +0900

    add testing laravel

---
+++ laravel/app/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Testing</title>
+	<style>
+	</style>
+</head>
+<body>
+	<h1>テスト</h1>
+	<div>{{ $SERVER_INSTANCE_ID or "None" }}</div>
+</body>
+</html>
